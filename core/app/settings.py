765d6dbb6d878f6da6f06994ac1259311376f547
Reversep <adilet.t.k@gmail.com>
2025-07-02 16:37:44 +0600

    change path for settings

--- core/app/settings.py
+++ core/app/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'core.app.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'core.app.wsgi.application'
 
 
 # Database
